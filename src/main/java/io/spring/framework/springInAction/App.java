daf67b89432b288a7481f96a6c138cc47aa5eeac
imalexyang <imalex@163.com>
2018-04-12 08:36:41 +0800

    init

---
+++ src/main/java/io/spring/framework/springInAction/App.java
@@ -0,0 +1,13 @@
+package io.spring.framework.springInAction;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
